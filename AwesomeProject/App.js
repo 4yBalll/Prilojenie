377178c8f06d30763e3fab019f7988c2d246c307
4yBalll <andrey.al121004@mail.ru>
2024-04-26 15:56:17 +0300

    Удалил лишний файл, поправил код

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Provider } from 'react-redux'; // Импорт Provider из react-redux
-import { configureStore } from '@reduxjs/toolkit'; // Импорт configureStore из Redux Toolkit
-import rootReducer from './screens/reducers'; // Импорт корневого редьюсера
+import { Provider } from 'react-redux'; 
+import { configureStore } from '@reduxjs/toolkit'; 
+import rootReducer from './screens/reducers'; 
 import Authorization from './screens/Authorization';
 import HomeScreen from './screens/HomeScreen';
 
 const Stack = createStackNavigator();
 
-// Создаем Redux store с использованием Redux Toolkit
+
 const reduxStore = configureStore({
-  reducer: rootReducer, // Передаем корневой редьюсер
-  // Дополнительные параметры конфигурации, если необходимо
+  reducer: rootReducer, 
+  
 });
 
 const App = () => {
